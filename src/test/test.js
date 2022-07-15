3cd5f20f58d6121d3657ea8821b3d30e587dc2bb
Arif Dian Suranta <arifdian000@gmail.com>
2022-07-15 15:02:46 +0700

    just test using mocha

--- src/test/test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-fs.readFile('../stub/StubController.stub', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
